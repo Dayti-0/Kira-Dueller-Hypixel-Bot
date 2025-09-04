e4d567cbe7802049c2ba5852d6f0fa2801d7f694
Nix <neogame34@gmail.com>
2025-09-04 21:03:16 +0200

    Use Bow feature in Blitz for long-range

--- src/main/kotlin/best/spaghetcodes/kira/bot/bots/Blitz.kt
+++ src/main/kotlin/best/spaghetcodes/kira/bot/bots/Blitz.kt
@@ -2,6 +2,7 @@ package best.spaghetcodes.kira.bot.bots
 
 import best.spaghetcodes.kira.bot.BotBase
 import best.spaghetcodes.kira.bot.features.MovePriority
+import best.spaghetcodes.kira.bot.features.Bow
 import best.spaghetcodes.kira.bot.player.Combat
 import best.spaghetcodes.kira.bot.player.Inventory
 import best.spaghetcodes.kira.bot.player.Mouse
@@ -10,7 +11,7 @@ import best.spaghetcodes.kira.utils.*
 import net.minecraft.init.Blocks
 import net.minecraft.util.Vec3
 
-class Blitz : BotBase("/play duels_blitz_duel"), MovePriority {
+class Blitz : BotBase("/play duels_blitz_duel"), MovePriority, Bow {
 
     override fun getName(): String = "Blitz"
 
@@ -97,7 +98,13 @@ class Blitz : BotBase("/play duels_blitz_duel"), MovePriority {
             when {
                 distance > 8f -> {
                     // Essayer arc/proj à longue distance
-                    if (Inventory.setInvItem("bow")) {
+                    if (distance > 5f) {
+                        useBow(distance)
+                        lastKitSwitch = now
+                    } else {
+                        if (!Inventory.setInvItem("rod")) {
+                            Inventory.setInvItem("sword")
+                        }
                         lastKitSwitch = now
                     }
                 }
