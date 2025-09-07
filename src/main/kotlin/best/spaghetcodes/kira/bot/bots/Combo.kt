6add0739e9ef909be9915937c2a0822fce6292c2
Nix <neogame34@gmail.com>
2025-09-07 15:00:27 +0200

    Refine forward movement radius

--- src/main/kotlin/best/spaghetcodes/kira/bot/bots/Combo.kt
+++ src/main/kotlin/best/spaghetcodes/kira/bot/bots/Combo.kt
@@ -306,7 +306,7 @@ class Combo : BotBase("/play duels_combo_duel"), MovePriority, Gap, Potion {
         }
 
         // Avance/stop
-        if (distance < 1.5f || (distance < 2.4f && combo >= 1)) {
+        if (distance < 0.8f) {
             Movement.stopForward()
         } else if (!tapping) {
             Movement.startForward()
